a815d4ca18cf1d29fb15979f84439849822de76d
David Pravst <david.pravst@gmail.com>
2016-09-23 23:23:52 +0200

    Prvi commit

---
+++ pretvori-string.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Smartninja Web Development 1 / Pogoji in zanke
+Domača naloga 8.3: Pretvori string v male črke
+Pretvori vse črke v male črke.
+"""
+
+stavek = raw_input("Vnesi stavek: ")
+print(stavek.lower())
